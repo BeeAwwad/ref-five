ae65d0208b949bb9741bd254323531a96b559383
BeeAwwad <aosalihu@gmail.com>
2026-07-26 21:52:38 +0100

    remove pwa vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,38 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
-      manifest: {
-        name: "Ref Five",
-        short_name: "Ref-5",
-        description: "Football refree application",
-        theme_color: "#000000",
-        background_color: "#ffffff",
-        display: "standalone",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
